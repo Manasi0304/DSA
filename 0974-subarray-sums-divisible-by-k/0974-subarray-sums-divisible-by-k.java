d756d7e71c4e4bb7c68c38110172f31706a3a5f9
Manasi Bharati <146508449+Manasi0304@users.noreply.github.com>
2024-08-11 22:54:34 +0530

    Time: 26 ms (16.51%), Space: 47 MB (67.06%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -1,16 +1,13 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
          Map<Integer, Integer> remainderCount = new HashMap<>();
-        remainderCount.put(0, 1); // To account for subarrays that start from the beginning and are divisible by k
-
+        remainderCount.put(0, 1); 
         int count = 0;
         int prefixSum = 0;
 
         for (int num : nums) {
             prefixSum += num;
             int remainder = prefixSum % k;
-
-            // Adjust remainder to be positive
             if (remainder < 0) {
                 remainder += k;
             }
