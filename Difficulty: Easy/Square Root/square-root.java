db32190f041dbb9e1f7e83a48384547f0d18b64f
Manasi Bharati <manasibharati94@gmail.com>
2025-01-31 23:01:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square Root/square-root.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+
+class SquartRoot {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int a = sc.nextInt();
+            Solution obj = new Solution();
+            System.out.println(obj.floorSqrt(a));
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*You are required to complete
+this function*/
+
+// Function to find square root
+// x: element to find square root
+class Solution {
+    int floorSqrt(int n) {
+        // Your code here
+        if (n == 1) return 1; 
+        
+        int low = 1, high = n, ans = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long square = (long) mid * mid; 
+            
+            if (square == n) {
+                return mid; 
+            } else if (square < n) {
+                ans = mid;
+                low = mid + 1; 
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
