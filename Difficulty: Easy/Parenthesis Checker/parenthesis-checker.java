45342a79dc3551f75f9e3805fe142ce5d17eb3ac
Manasi Bharati <manasibharati94@gmail.com>
2025-03-01 21:55:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Driverclass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // Reading total number of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // reading the string
+            String st = sc.next();
+
+            // calling ispar method of Paranthesis class
+            // and printing "balanced" if it returns true
+            // else printing "not balanced"
+            if (new Solution().isBalanced(st) == true)
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution {
+    static boolean isBalanced(String s)
+    {
+        // code here
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);  
+            }
+            else 
+            {
+                if (st.isEmpty()) 
+                {
+                    return false; 
+                }
+                char top = st.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) 
+                {
+                    return false; 
+                }
+            }
+        }
+    
+        
+        return st.isEmpty(); 
+        
+    }
+}
